bea22c9ba9b4995be7fa45757936bb7a29ff7f9e
Alfred <alfredmp6@hotmail.com>
2022-06-16 12:35:53 +0200

    guardado esquema principal menos form

--- shoppingjs.js
+++ shoppingjs.js
@@ -2,3 +2,8 @@ function muestraForm() {
     let divgost=document.getElementById('formprod')
     divgost.style.display="flex"
 }
+function cierraForm() {
+
+    let divgost=document.getElementById('formprod')
+    divgost.style.display="none"
+}
