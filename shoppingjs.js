7d5baa09282e2a0f7c2b48f870f6a0e0ac2daa74
Alfred <alfredmp6@hotmail.com>
2022-06-17 12:32:44 +0200

    guardado de toda la mañana

--- shoppingjs.js
+++ shoppingjs.js
@@ -7,3 +7,17 @@ function cierraForm() {
     let divgost=document.getElementById('formprod')
     divgost.style.display="none"
 }
+/*
+function muestraCantidad() {
+    let cantidad = document.getElementById("cantidad").value;
+  document.getElementById("mostrador").innerHTML = cantidad;
+
+}
+*/
+/*poniendo entre parentesis esos valores estamos indicando al programador que reutilice la funcion que necesitamos dos id para que la 
+funcion reaccione,despues en la funcion hemos puesto nuestras id donde tienen que interactuar los alores dados por el programador  */
+function muestraCantidad(idleer,idmostrar){
+    let cantidad = document.getElementById(idleer).value;
+    document.getElementById(idmostrar).innerHTML = cantidad;
+}
+
